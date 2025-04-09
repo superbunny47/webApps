77f5a975adc9f595b6c775c85aa2c61c75b6bdd3
superbunny47 <sh968@sussex.ac.uk>
2025-04-09 17:37:39 +0100

    Update settings.py

--- webapps2025/settings.py
+++ webapps2025/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a+((q4ycc5ubzdd!=@*#o=ntr$$c*m!9luxvsd8t+0m%-tg&pi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["ec2-44-202-168-195.compute-1.amazonaws.com"]
 SITE_URL = "http://127.0.0.1:8000"
 
 # Application definition
